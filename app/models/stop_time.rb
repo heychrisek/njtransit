c033a9a9b009a9297c93bab5b8004a703fb2b347
chriskohlbrenner <c.kohlbrenner@gmail.com>
2014-03-26 09:07:28 -0400

    progress on StopTime model - getting next departure

--- app/models/stop_time.rb
+++ app/models/stop_time.rb
@@ -1,2 +1,17 @@
 class StopTime < ActiveRecord::Base
+  belongs_to :trip
+  belongs_to :stop
+
+  def self.all_origin_departures(origin)
+    origin.upcase!
+    where("stop_id = 38174").joins(:trip).where('trip_headsign = ?', origin).order(:departure_time)
+  end
+
+
+
+  def self.next_origin_departure(origin)
+    self.all_origin_departures.#order #(:departure_time - Time.now)
+    .where(departure_time between Time.now and two hours later)
+  end
+
 end
